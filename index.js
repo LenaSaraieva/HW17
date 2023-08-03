7df75db385d6c56a9024d0f8a60a4c63ba1ce31e
LenaSaraieva <lena.saraeva1904@gmail.com>
2023-08-03 21:18:20 +0300

    task with balance

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //Дан масив об'єктів. Вивести масив телефонних номерів користувачів, у яких баланс більше 2000 доларів. І знайти суму всіх балансів користувачів
+let users = [];
+const usersWithBalanceOver2000 = users.filter(user => {
+    const balance = parseFloat(user.balance.replace(/[^0-9.-]+/g,""));
+    return balance > 2000;
+  });
+
+const phoneNumbers = usersWithBalanceOver2000.map(user => user.phone);
+console.log("Phone numbers with balance over $2000:", phoneNumbers);
+
+const totalBalance = users.reduce((sum, user) => {
+    const balance = parseFloat(user.balance.replace(/[^0-9.-]+/g,""));
+    return sum + balance; 
+}, 0);
+
+    console.log("Total balance of all users:", totalBalance);
